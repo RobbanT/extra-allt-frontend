3f9b6670037f86db7f86ce7042647c0a3fd009cb
Roberto Tencic <roberto.tencic@outlook.com>
2024-10-17 22:44:41 +0200

    Färdigt.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { ProductsComponent } from '../products/products.component';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
+import { OrdersComponent } from '../orders/orders.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductsComponent,
+    LoginComponent,
+    RegisterComponent,
+    OrdersComponent,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
 export class MainComponent {
+  title = input();
+  changedTitle = output<string>();
+  changedUserLoggedIn = output<boolean>();
 
+  changeUserLoggedIn(userloggedIn: boolean) {
+    this.changedUserLoggedIn.emit(userloggedIn);
+  }
+
+  changeTitle(title: string) {
+    this.changedTitle.emit(title);
+  }
 }
